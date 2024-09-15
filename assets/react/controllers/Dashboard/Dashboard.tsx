241964c8e730395cab2737ff5e96d76ee86c3708
RobinP <poncon.robin@gmail.com>
2024-09-16 00:09:59 +0200

    add btn add animal + create add and edit methods

--- assets/react/controllers/Dashboard/Dashboard.tsx
+++ assets/react/controllers/Dashboard/Dashboard.tsx
@@ -6,40 +6,154 @@ type ShowAnimalsProps = {
 	animals: Animal[];
 };
 
+type FlashMessage = {
+	type: "success" | "error";
+	message: string;
+};
+
 const Dashboard: React.FC<ShowAnimalsProps> = ({ animals }) => {
 	const [openModal, setOpenModal] = useState<boolean>(false);
-	// const [animal]
+	const [animalSelected, setAnimalSelected] = useState<Animal | null>(null);
+	const [flashMessage, setFlashMessage] = useState<FlashMessage | null>(null);
+
+	const openDeleteConfirmationModal = (animal: Animal) => {
+		setAnimalSelected(animal);
+		setOpenModal(true);
+	};
+
+	const cancelDelete = () => {
+		setAnimalSelected(null);
+		setOpenModal(false);
+	};
+
+	const deleteAnimal = async () => {
+		if (animalSelected) {
+			try {
+				const response = await fetch(`/animal/supprimer/${animalSelected.id}`, {
+					method: "DELETE",
+					headers: {
+						"Content-Type": "application/json"
+					}
+				});
+
+				if (response.ok) {
+					window.scrollTo(0, 0);
+					setFlashMessage({
+						type: "success",
+						message: `L'animal ${animalSelected.name} a bien été supprimé !`
+					});
+					setAnimalSelected(null);
+					setOpenModal(false);
+				} else {
+					setFlashMessage({
+						type: "error",
+						message: `Une erreur est survenue, l'animal ${animalSelected.name} n'a pas été supprimé !`
+					});
+				}
+			} catch (error) {
+				setFlashMessage({
+					type: "error",
+					message: `Une erreur est survenue, l'animal ${animalSelected.name} n'a pas été supprimé !`
+				});
+			}
+		}
+	};
 
 	return (
-		<div>
-			{openModal && (
-				<div>
-					<h2>Êtes-vous sûr de vouloir supprimer ?</h2>
+		<div className="w-full px-4 pb-10">
+			{animalSelected && openModal && (
+				<div className="fixed inset-0 flex items-center justify-center bg-gray-800 bg-opacity-50 z-10">
+					<div className="bg-white p-5 rounded-lg shadow-lg w-1/3 max-w-lg">
+						<h2>Êtes-vous sûr de vouloir supprimer {animalSelected.name} ?</h2>
+						<div className="mt-4 flex justify-end space-x-4">
+							<button
+								onClick={cancelDelete}
+								className="px-4 py-2 bg-gray-300 rounded-md hover:bg-gray-400"
+							>
+								Annuler
+							</button>
+							<button
+								onClick={deleteAnimal}
+								className="px-4 py-2 bg-red-500 text-white rounded-md hover:bg-red-600"
+							>
+								Supprimer
+							</button>
+						</div>
+					</div>
 				</div>
 			)}
-			<table>
-				<thead>
-					<tr>
-						<th>Nom</th>
-						<th>Type</th>
-						<th>Race</th>
-						<th>Prix</th>
-						<th>Actions</th>
-					</tr>
-				</thead>
-				<tbody>
-					{animals.map((animal) => (
-						<tr key={animal.id}>
-							<td>{animal.name}</td>
-							<td>{animal.type}</td>
-							<td>{animal.breed}</td>
-							<td>{formattedPrice(animal.price)}</td>
-							<a href={`/animal/${animal.id}/editer`}>Éditer</a>
-							<button onClick={() => setOpenModal(true)}>Supprimer</button>
+			<div className="flex justify-center gap-4 pb-10">
+				<h2 className="text-center text-lg my-auto font-semibold">
+					Envie d'ajouter un nouvel animal ?
+				</h2>
+				<a
+					className="flex py-2 px-3 gap-2 rounded-md bg-gray-700 text-white hover:bg-gray-900"
+					href="/animal/creer"
+				>
+					Ajouter
+					<img
+						className="w-6 h-6"
+						src="/icons/icon_add.svg"
+						alt="icon add"
+					/>
+				</a>
+			</div>
+			<div className="w-fit mx-auto">
+				{flashMessage && (
+					<div
+						className={`py-2 px-4 mb-5 rounded-md text-center ${
+							flashMessage.type === "success" ? "bg-green-200" : "bg-red-200"
+						}`}
+					>
+						<p>{flashMessage.message}</p>
+					</div>
+				)}
+				<table className="bg-white border-2 border-gray-300 rounded-md">
+					<thead className="rounded-t-md">
+						<tr>
+							<th className="border-2 py-2 px-3 border-gray-300">Nom</th>
+							<th className="border-2 py-2 px-3 border-gray-300">Type</th>
+							<th className="border-2 py-2 px-3 border-gray-300">Race</th>
+							<th className="border-2 py-2 px-3 border-gray-300">Prix</th>
+							<th className="border-2 py-2 px-3 border-gray-300">Actions</th>
 						</tr>
-					))}
-				</tbody>
-			</table>
+					</thead>
+					<tbody className="rounded-b-md">
+						{animals.map((animal) => (
+							<tr
+								key={animal.id}
+								className="odd:bg-gray-100"
+							>
+								<td className="p-4  border-2 border-gray-300">{animal.name}</td>
+								<td className="p-4 border-2 border-gray-300">{animal.type}</td>
+								<td className="p-4 border-2 border-gray-300">{animal.breed}</td>
+								<td className="p-4 border-2 border-gray-300">
+									{formattedPrice(animal.price)}
+								</td>
+								<td className="p-4 flex gap-4">
+									<a
+										className="flex gap-2 bg-blue-600 text-white rounded-md py-2 px-3 hover:bg-blue-700"
+										href={`/animal/${animal.id}/editer`}
+									>
+										<img
+											className="w-6 h-6"
+											src="/icons/icon_edit.svg"
+											alt="icon edit"
+										/>
+										Éditer
+									</a>
+									<button
+										className="bg-red-600 text-white rounded-md py-2 px-3 hover:bg-red-700"
+										onClick={() => openDeleteConfirmationModal(animal)}
+									>
+										Supprimer
+									</button>
+								</td>
+							</tr>
+						))}
+					</tbody>
+				</table>
+			</div>
 		</div>
 	);
 };
--- src/Controller/AnimalController.php
+++ src/Controller/AnimalController.php
@@ -6,6 +6,9 @@ use App\Entity\Animal;
 use App\Service\AnimalFormatter;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Bundle\SecurityBundle\Security;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
@@ -41,6 +44,84 @@ class AnimalController extends AbstractController
         return $this->render("animal/showAnimal.html.twig", [
             "animal" => $formatAnimal
         ]);
+    }
+
+    #[Route("/animal/supprimer/{id}", name: "delete_animal", methods: ["DELETE"])]
+    public function deleteAnimal($id, EntityManagerInterface $em, Security $security): JsonResponse
+    {
+        if (!$security->isGranted('ROLE_ADMIN')) {
+            throw $this->createAccessDeniedException('Accès refusé.');
+        }
+
+        $animal = $em->getRepository(Animal::class)->find($id);
+
+        if (!$animal) {
+            return new JsonResponse(["status" => "Animal not found"], 404);
+        }
+
+        $em->remove($animal);
+        $em->flush();
+
+        return new JsonResponse(["status" => "success"], 200);
+    }
+
+    #[Route('/animal/creer', name: 'create_animal', methods: ["POST"])]
+    public function createAnimal(EntityManagerInterface $em, Request $request, Security $security) {
+        if (!$security->isGranted('ROLE_ADMIN')) {
+            throw $this->createAccessDeniedException('Accès refusé.');
+        }
+
+        $data = json_decode($request->getContent(), true);
+
+        if (!$data["name"] || !$data["age"] || !$data["type"] || !$data["breed"] || !$data["price"]) {
+            return new JsonResponse(['message' => 'Erreur, il manque des données obligatoires.'], 400);
+        }
+
+        $animal = new Animal();
+        $animal->setName($data["name"]);
+        $animal->setAge($data["age"]);
+        $animal->setType($data["type"]);
+        $animal->setBreed($data["breed"]);
+        $animal->setDescription($data["description"] ?? null);
+        $animal->setPrice($data["price"]);
+        $animal->setPhotos($data["photos"] ?? null);
+
+        $em->persist($animal);
+        $em->flush();
+
+        return new JsonResponse(['status' => 'success'], 200);
+    }
+
+    #[Route('/animal/{id}/editer', name: 'edit_animal', methods: ["POST"])]
+    public function editAnimal($id, Request $request, EntityManagerInterface $em, Security $security): JsonResponse {
+        if (!$security->isGranted('ROLE_ADMIN')) {
+            throw $this->createAccessDeniedException('Accès refusé.');
+        }
+
+        $animal = $em->getRepository(Animal::class)->find($id);
+
+        if (!$animal) {
+            return new JsonResponse(['message' => 'Animal not found'], 404);
+        }
+
+        $data = json_decode($request->getContent(), true);
+
+        if (!$data["name"] || !$data["age"] || !$data["type"] || !$data["breed"] || !$data["price"]) {
+            return new JsonResponse(['message' => 'Erreur, il manque des données obligatoires.'], 400);
+        }
+
+        $animal->setName($data["name"]);
+        $animal->setAge($data["age"]);
+        $animal->setType($data["type"]);
+        $animal->setBreed($data["breed"]);
+        $animal->setDescription($data["description"] ?? null);
+        $animal->setPrice($data["price"]);
+        $animal->setPhotos($data["photos"] ?? null);
+
+        $em->persist($animal);
+        $em->flush();
+
+        return new JsonResponse(['status' => 'success'], 200);
     }
 }
 

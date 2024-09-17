9f3fa92f20021a0f153d01bfa52603010f0f8535
RobinP <poncon.robin@gmail.com>
2024-09-17 17:15:52 +0200

    add 404 error page

---
+++ src/EventListener/ExceptionListener.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\EventListener;
+
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Event\ExceptionEvent;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+use Twig\Environment;
+
+class ExceptionListener
+{
+    private $twig;
+
+    public function __construct(Environment $twig)
+    {
+        $this->twig = $twig;
+    }
+
+    public function onKernelException(ExceptionEvent $event)
+    {
+        $exception = $event->getThrowable();
+
+        if ($exception instanceof NotFoundHttpException) {
+            $response = new Response(
+                $this->twig->render('errors/404.html.twig'),
+                Response::HTTP_NOT_FOUND
+            );
+
+            $event->setResponse($response);
+        }
+    }
+}
